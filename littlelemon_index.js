eb8199737dc81d47f9dd10c315836d3c684b4862
khelgren12 <117606118+khelgren12@users.noreply.github.com>
2024-04-10 13:44:07 -0600

    Add files via upload

---
+++ littlelemon_index.js
@@ -0,0 +1,7 @@
+<Routes>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/booking" element={<Reservations />}></Route>
+    <Route path="/" element={<Order Online />}></Route>
+    <Route path="/" element={<Menu />}></Route>
+    <Route path="/" element={<Login /> }></Route>
+</Routes>
